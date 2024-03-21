43f3217911a78e432639e25a0cefdd8483634fa9
Nicole Torres <nicolejoan9@yahoo.com>
2024-03-21 11:00:14 -0400

    added another section to landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/home.jsx";
 import Location from "./pages/location.jsx";
 import Landing from "./pages/landing.jsx";
 import Testimonials from "./pages/testimonials.jsx"
+import Features from "./pages/features.jsx";
+import Plan from "./pages/plan.jsx";
 // import Projects from "./pages/projects.jsx";
 // import Contact from "./pages/contact.jsx";
 // import Interests from "./pages/interests.jsx";
@@ -17,6 +19,8 @@ function App() {
       <Home />
       <Landing />
       <Testimonials />
+      <Features />
+      <Plan />
       <Routes>
         <Route path="/location" element={<Location />}  />
       </Routes>
---
+++ client/src/pages/features.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./css/features.css";
+
+
+function Features() {
+  return (
+    <div className="homePage4">
+      <p className="homePage4P1">Features to replace all your other tools</p>;
+    </div>
+  );
+}
+
+export default Features;
+
+
+
